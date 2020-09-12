884d644ddf1ff18fe736bbf01e1d529620660bad
maaadc <maaadc@gmail.com>
2020-09-12 22:35:51 +0200

    Quick & dirty working audio, cannot handle ID3 cover art

--- singsang.ino
+++ singsang.ino
@@ -1,12 +1,28 @@
 #include <M5Core2.h>
+#include <SD.h>
+#include <WiFi.h>
+
+#include "src/SingSangPlayer.hpp"
 
 void setup()
 {
     M5.begin();
-    SD.begin();
+    //M5.Axp.SetLed(0U);
 
-    M5.Lcd.setBrightness(100);
-    M5.Lcd.drawPngFile(SD, "/logo.png", 40, 0, 240, 240, 0, 0, 240. / 512, 127);
+    WiFi.mode(WIFI_OFF);
+    delay(500);
+
+    M5.Lcd.setTextFont(2);
+    M5.Lcd.println("Hello, sing sang.");
+    //M5.Lcd.drawPngFile(SD, "/logo.png", 0, 0, 120, 120, 0, 0, 120./512.);
+
+    //delay(1000);
+
+    singsang.setVolume(0.3);
+    singsang.playMp3("/test2.mp3");
 }
 
-void loop() {}
+void loop()
+{
+    singsang.loopPlay();
+}
