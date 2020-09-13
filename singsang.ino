11b9b370101cd56b2428c0d84870e5c1435e4d8e
maaadc <maaadc@gmail.com>
2020-09-13 22:39:57 +0200

    Switched audio lib, basic mp3 replay working

--- singsang.ino
+++ singsang.ino
@@ -2,12 +2,16 @@
 #include <SD.h>
 #include <WiFi.h>
 
-#include "src/SingSangPlayer.hpp"
+#include "Audio.h"
+Audio audio;
+
+//#include "src/SingSangPlayer.hpp"
 
 void setup()
 {
     M5.begin();
-    //M5.Axp.SetLed(0U);
+    M5.Axp.SetLed(false);
+    M5.Axp.SetSpkEnable(true);
 
     WiFi.mode(WIFI_OFF);
     delay(500);
@@ -17,12 +21,34 @@ void setup()
     //M5.Lcd.drawPngFile(SD, "/logo.png", 0, 0, 120, 120, 0, 0, 120./512.);
 
     //delay(1000);
+    audio.setPinout(12, 0, 2);
 
+    audio.setVolume(5);
+
+    audio.connecttoSD("test3.mp3");
     singsang.setVolume(0.3);
     singsang.playMp3("/test2.mp3");
 }
 
 void loop()
 {
-    singsang.loopPlay();
+    audio.loop();
+}
+
+void audio_info(const char *info)
+{
+    Serial.print("info        ");
+    Serial.println(info);
+}
+
+void audio_id3data(const char *info)
+{ //id3 metadata
+    Serial.print("id3data     ");
+    Serial.println(info);
+}
+
+void audio_eof_mp3(const char *info)
+{ //end of file
+    Serial.print("eof_mp3     ");
+    Serial.println(info);
 }
